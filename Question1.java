54390656966198c6ed218940b967548efe317c30
MatthewGarcia5 <71661372+MatthewGarcia5@users.noreply.github.com>
2020-10-08 10:37:47 -0400

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 
-
-class Question1 {
+class Math {
     public static void main(String[] args) {
 
-        /* Write your code here */
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Put in a 2 digit number");
+        String x = scan.nextLine();
+        System.out.println(x.charAt(0));
+        System.out.println(x.charAt(1));
 
     }
 }
